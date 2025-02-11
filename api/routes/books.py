a136a0cbf09350b874799c64dd1e6d23b8bc0198
theglad-x <ugerondu@gmail.com>
2025-02-11 19:03:02 +0100

    Added endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -47,6 +47,12 @@ async def create_book(book: Book):
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.get_book(book_id)
+    if book:
+        return book
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
